6a44a16a8e8ad54e1d516a3b8592609a263d500e
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2022-10-03 21:27:42 +0200

    added fetch

--- quotes.cr
+++ quotes.cr
@@ -1,7 +1,10 @@
 ##### Imports #####
 require "option_parser";
+require "json";
+require "http/client";
 
 ##### Functions #####
+## Parse args ##
 OptionParser.parse do |parser|
     parser.banner = "Quotes - by NewDawn0"
     parser.on "-v", "--version", "Show version" do
@@ -13,5 +16,16 @@ OptionParser.parse do |parser|
         puts parser
         exit
     end
+    parser.on "-r", "--random", "Show random quote" do
+        fetch
+    end
 end
-
+## fn fetch ##
+def fetch
+    res = HTTP::Client.get "https://stoicquotesapi.com/v1/api/quotes/random"
+    res = JSON.parse(res.body)
+    quote = res["body"]
+    author = res["author"]
+    puts "#{quote}\t -- #{author}"
+    exit
+end
